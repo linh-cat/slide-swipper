48b54b2754a9c78bab4cec918eeb991a976f12b4
Thanh Linh <linh.thanh@wonderlabs.io>
2021-07-14 11:35:06 +0700

    initial slide

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css";
+
+// import Swiper core and required modules
+import SwiperCore, { Pagination } from "swiper/core";
+
+// install Swiper modules
+SwiperCore.use([Pagination]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Swiper
+        slidesPerView={'auto'} spaceBetween={40}  
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        className="mySwiper"
+      >
+        <SwiperSlide><img src="/images/1.jpg" /></SwiperSlide>
+        <SwiperSlide><img src="/images/2.jpg" /></SwiperSlide>
+        <SwiperSlide><img src="/images/3.jpg" /></SwiperSlide>
+        <SwiperSlide><img src="/images/1.jpg" /></SwiperSlide>
+        <SwiperSlide><img src="/images/2.jpg" /></SwiperSlide>
+       
+      </Swiper>
+
+      {/* <div class="swiper-container mySwiper">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">Slide 1</div>
+          <div class="swiper-slide">Slide 2</div>
+          <div class="swiper-slide">Slide 3</div>
+          <div class="swiper-slide">Slide 4</div>
+          <div class="swiper-slide">Slide 5</div>
+          <div class="swiper-slide">Slide 6</div>
+          <div class="swiper-slide">Slide 7</div>
+          <div class="swiper-slide">Slide 8</div>
+          <div class="swiper-slide">Slide 9</div>
+        </div>
+        <div class="swiper-pagination"></div>
+      </div> */}
+    </>
   );
 }
 
